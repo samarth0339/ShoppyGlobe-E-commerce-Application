3cc7f82a41e062394c260e5c1e3f93bf2b70114d
samarth0339 <samarthkasture0339@gmail.com>
2025-09-20 15:18:06 +0530

    feat: implement search slice for product filtering state

---
+++ src/store/slices/searchSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  searchTerm: '',
+};
+
+const searchSlice = createSlice({
+  name: 'search',
+  initialState,
+  reducers: {
+    setSearchTerm: (state, action) => {
+      state.searchTerm = action.payload;
+    },
+    clearSearch: (state) => {
+      state.searchTerm = '';
+    },
+  },
+});
+
+export const { setSearchTerm, clearSearch } = searchSlice.actions;
+
+export const selectSearchTerm = (state) => state.search.searchTerm;
+
+export default searchSlice.reducer;
